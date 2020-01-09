1122c3a4aa48fe1caa2cefc7e84d08e89b15de39
Pedro Moura <pedromoura@MacBook-Pro-de-Pedro.local>
2020-01-08 23:44:23 -0300

    Commit Initial

---
+++ app/src/main/java/br/com/pedrohenriquemoura/notas/MainActivity.kt
@@ -0,0 +1,29 @@
+package br.com.pedrohenriquemoura.notas
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_principal, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item!!.itemId == R.id.action_adicionar) {
+            //TODO: Chamar tela de adicionar nova nota
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
